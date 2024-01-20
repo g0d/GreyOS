670aaa1c0e72623fee7c418129e8fb8879904004
takhstzivatos <157225297+takhstzivatos@users.noreply.github.com>
2024-01-20 14:41:40 +0000

    Improving Code Quality/Escape dangerous functions

--- Code/framework/extensions/php/user/i_fb/chat/languages/English/English_global.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-define("LAN_PLUGIN__FB_CHAT_NAME", "FB Chat");
-define("LAN_PLUGIN__FB_CHAT_SUMM", "Facebook styled chat");
-define("LAN_PLUGIN__FB_CHAT_DESC", "Facebook styled chat...");
-define("LAN_PLUGIN__FB_CHAT_LINK", "");
-
-?>
