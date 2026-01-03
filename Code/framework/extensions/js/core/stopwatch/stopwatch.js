ec3391631da5886ca32a4d62ecbe33f887c3a774
George Delaportas (ViR4X) <g.delaportas@gmail.com>
2026-01-03 02:06:09 +0200

    Upgrade Stopwatch extension
    Essentially enable an internal model to store the last known config so that next time you start the time you won't have to pass all arguments again and again. This also improves or rather avoids cases and scenarios where you would treat the timer and it's callback as a thread. Now you have better control and precise timing on start stop events without asynchronously checking guard flags to return and other thread-techniques.

--- Code/framework/extensions/js/core/stopwatch/stopwatch.js
+++ Code/framework/extensions/js/core/stopwatch/stopwatch.js
@@ -1,18 +1,25 @@
 /*
     Stopwatch (High precision timer for JS)
 
-    File name: stopwatch.js (Version: 0.6)
+    File name: stopwatch.js (Version: 0.7)
     Description: This file contains the Stopwatch extension.
     Dependencies: Vulcan.
 
     Coded by George Delaportas (G0D/ViR4X)
-    Copyright (C) 2017 - 2022
+    Copyright (C) 2017 - 2026
     Open Software License (OSL 3.0)
 */
 
 // Stopwatch (High precision timer)
 function stopwatch()
 {
+    function config_model()
+    {
+        this.interval = null;
+        this.callback = null;
+        this.run_once = null;
+    }
+
     function instance(interval, callback)
     {
         if (!is_on)
@@ -22,7 +29,7 @@ function stopwatch()
 
         callback.call(this, self);
 
-        if (is_one_shot)
+        if (config.run_once)
         {
             is_on = false;
 
@@ -35,20 +42,28 @@ function stopwatch()
         timer_handler = setTimeout(function() { instance(interval, callback); }, (interval - diff));
     }
 
-    this.start = function(interval, callback, run_once)
+    this.start = function(interval = null, callback = null, run_once = null)
     {
         if (is_on)
             return false;
 
-        if (!utils.validation.numerics.is_integer(interval) || interval < 1 || 
-            !utils.validation.misc.is_function(callback) || 
-            (!utils.validation.misc.is_undefined(run_once) && !utils.validation.misc.is_bool(run_once)))
-            return false;
+        if (config.interval !== null && interval === null && callback === null && run_once === null)
+            timer_handler = setTimeout(function() { instance(config.interval, config.callback); }, config.interval);
+        else
+        {
+            if (!utils.validation.numerics.is_integer(interval) || interval < 1 || 
+                !utils.validation.misc.is_function(callback) || 
+                (!utils.validation.misc.is_undefined(run_once) && !utils.validation.misc.is_bool(run_once)))
+                return false;
 
-        timer_handler = setTimeout(function() { instance(interval, callback); }, interval);
+            config.interval = interval;
+            config.callback = callback;
+            config.run_once = run_once;
+
+            timer_handler = setTimeout(function() { instance(config.interval, config.callback); }, config.interval);
+        }
 
         is_on = true;
-        is_one_shot = run_once;
 
         return true;
     };
@@ -67,10 +82,10 @@ function stopwatch()
 
     var self = this,
         is_on = false,
-        is_one_shot = false,
         init_time = new Date().getTime(),
         delay = 0,
         diff = 0,
         timer_handler = null,
+        config = new config_model(),
         utils = new vulcan();
 }
